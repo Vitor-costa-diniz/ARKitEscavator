1bd4ad7e27508b2350796937157bc889a3a3a881
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2025-06-12 10:28:16 -0300

    create project

---
+++ ARKitEscavator/ARKitEscavator/ARKitEscavatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitEscavatorApp.swift
+//  ARKitEscavator
+//
+//  Created by Vitor Costa on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ARKitEscavatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
