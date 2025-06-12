0a9daa9b44756619d40163076230219af93c4e48
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2025-06-12 10:51:57 -0300

    create location manager

---
+++ ARKitEscavator/ARKitEscavator/Managers/Location/LocationManager.swift
@@ -0,0 +1,39 @@
+//
+//  LocationManager.swift
+//  ARKitEscavator
+//
+//  Created by Vitor Costa on 12/06/25.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    
+    @Published var userLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        requestPermission()
+    }
+    
+    func requestPermission() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            DispatchQueue.main.async {
+                self.userLocation = location.coordinate
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Erro ao obter localização: \(error.localizedDescription)")
+    }
+}
