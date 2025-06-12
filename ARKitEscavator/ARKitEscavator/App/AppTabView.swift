019d93e19936083009d43907e7ae1cb258a3b70a
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2025-06-12 10:41:18 -0300

    setting iniital project

---
+++ ARKitEscavator/ARKitEscavator/App/AppTabView.swift
@@ -0,0 +1,29 @@
+//
+//  AppTabView.swift
+//  ARKitEscavator
+//
+//  Created by Vitor Costa on 12/06/25.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+    @State private var tabSelection: Int = 0
+    
+    var body: some View {
+        TabView(selection: $tabSelection) {
+            Tab("Mapa", systemImage: "map", value: 0) {
+                Text("Mapa")
+            }
+            
+            Tab("Bau", systemImage: "house", value: 1) {
+                Text("Bau")
+            }
+        }
+    }
+}
+
+
+#Preview {
+    AppTabView()
+}
---
+++ ARKitEscavator/ARKitEscavator/Utils/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ARKitEscavator
+//
+//  Created by Vitor Costa on 12/06/25.
+//
+
+import Foundation
