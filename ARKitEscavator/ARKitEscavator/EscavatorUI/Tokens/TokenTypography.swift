f457f3d330d7d141c4984ac4b0f435e347e47823
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2025-06-18 16:47:29 -0300

    Add font and font typograph

---
+++ ARKitEscavator/ARKitEscavator/EscavatorUI/Tokens/TokenTypography.swift
@@ -0,0 +1,32 @@
+//
+//  TokenTypography.swift
+//  ARKitEscavator
+//
+//  Created by Vitor Costa on 18/06/25.
+//
+
+import SwiftUI
+struct TokenTypography {
+
+    fileprivate enum FontSize: CGFloat {
+        case medium = 20
+    }
+
+    fileprivate enum FontType: String {
+        case regular = "LoveYaLikeASister-Regular"
+    }
+}
+
+enum TypographyCase {
+    /// Font: LoveYaLikeASister-Regular; Size: 20
+    case body2Regular
+}
+
+extension Font {
+    static func fourierUI(_ typographyCase: TypographyCase) -> Font {
+        switch typographyCase {
+        case .body2Regular:
+            return Font.custom(TokenTypography.FontType.regular.rawValue, size: TokenTypography.FontSize.medium.rawValue)
+        }
+    }
+}
